c63b822f9ff4f0755fed4d73be104e4880dce7c1
jhuzinha <jholeanasantos@gmail.com>
2026-07-09 12:11:15 -0300

    fix: documentações

--- webscraper/ansi_other.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows
-
-package main
-
-// enableWindowsANSI é um no-op fora do Windows: Linux e macOS já
-// interpretam sequências ANSI nativamente no terminal.
-func enableWindowsANSI() {}
